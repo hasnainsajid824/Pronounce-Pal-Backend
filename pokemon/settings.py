54939c492363f1b76030173c9f71c9a427ef86e9
Hussy24 <hasajid882@gmail.com>
2024-07-28 20:58:16 +0500

    mail server changed

--- pokemon/settings.py
+++ pokemon/settings.py
@@ -170,8 +170,8 @@ AUTH_USER_MODEL = 'accounts.User'
 EMAIL_BACKEND="django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'extrahussy22@gmail.com'
-EMAIL_HOST_PASSWORD = 'sxzv doms aqzf qypl'
+EMAIL_HOST_USER = 'pronouncepal.help@gmail.com'
+EMAIL_HOST_PASSWORD = 'qbph lrab pgrz fvjs'
 EMAIL_USE_TLS = True
 
 
